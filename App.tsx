a541aaeefec2575637217202d88fa2cdcdd857d5
cd-iqrash <123447894+cd-iqrash@users.noreply.github.com>
2024-04-25 23:48:56 +0500

    stack added

--- App.tsx
+++ App.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
-import {I18nextProvider} from 'react-i18next';
+import 'react-native-gesture-handler';
+import { I18nextProvider } from 'react-i18next';
 import i18n from './src/localization/18next/18n.config';
 import LoginPage from './src/screens/LoginPage';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LanguageScreen from './src/screens/ChangeLanguage';
 
 export default function App() {
-  return (
-    <I18nextProvider i18n={i18n}>
-      <NavigationContainer>
-        <LoginPage />
-      </NavigationContainer>
-    </I18nextProvider>
-  );
-}
+
+  const Stack = createStackNavigator();
+
+ 
+
+    function MyStack() {
+      return (
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={LoginPage} />
+          <Stack.Screen name="Language" component={LanguageScreen} />
+
+        </Stack.Navigator>
+      );
+    }
+    return (
+      <I18nextProvider i18n={i18n}>
+        <NavigationContainer>
+          <MyStack />
+        </NavigationContainer>
+      </I18nextProvider>
+    );
+  }
+
+
